7838232f6671bf2cf538d813a78cf3d1e41961c3
gollapalikarthik <gollapalikarthik6@gmail.com>
2026-09-23 22:41:30 +0530

    Create challenge01.c

---
+++ Challenge-01/challenge01.c
@@ -0,0 +1,30 @@
+//programe: to calculate a student marks
+//name:gollapali kasi venkata naga karthik
+//date: 23/09/2026
+#include<stdio.h>
+int main()
+{
+int english, phisics, maths, biology, chemistry;
+printf("To calculate student total marks, average marks and percentages given below\n");
+printf("Enter student english marks:\n"); //tadhsfhjaefnk
+scanf("%d", &english);
+printf("Enter student phisics marks:\n");
+scanf("%d", &phisics);
+printf("Enter student maths marks:\n");
+scanf("%d",&maths);
+printf("Enter student biology marks:\n");
+scanf("%d", &biology);
+printf("Enter student chemistry marks:\n");
+scanf("%d",&chemistry);
+
+int total=english+phisics+maths+biology+chemistry;
+printf("Out of total 500 marks a studenty secured grand total of: %d\n", total);
+
+float percentage=(total/500.0)*100;
+printf("The percentage secured by a student is: %.2f\n", percentage);
+
+int average =(total/5);
+printf("The average marks secured by a student on per subject is: %d\n", average);
+
+return 0;
+}
